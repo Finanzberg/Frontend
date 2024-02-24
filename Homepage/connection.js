053d31e1fd1b271e2925e13f8403d450582f21f8
NikMitzel <127861651+NikMitzel@users.noreply.github.com>
2024-02-24 22:54:36 +0100

    added login / register funktion

---
+++ Homepage/connection.js
@@ -0,0 +1,14 @@
+let api = "http://localhost:8080/api/v1/";
+
+function send(method, apiSection, data, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, api + apiSection, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText, xhr.status);
+        }
+    }
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Cookie", "session=" + getCookie("session"));
+    xhr.send(data);
+}
