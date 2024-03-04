3832d974470aac263c6225b8ef38ad9a47c09544
xLennnix <moritzmeissgeier@gmail.com>
2024-03-04 13:38:02 +0100

    Chart start

---
+++ Finanzberg/resources/js/chart-script.js
@@ -0,0 +1,29 @@
+    send("GET", "data/saldo", {}, function(response, status){
+        const data = JSON.parse(response)
+        createChart(data.labels, data.data)
+    })
+    
+    function createChart(labels, amounts) {
+        const ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar', // or 'line', 'pie', etc.
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Amounts',
+                    data: amounts,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+    
