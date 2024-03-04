38963c29f88ceac6eccd17955156518d901149d1
xLennnix <moritzmeissgeier@gmail.com>
2024-03-04 15:13:52 +0100

    Charts noch wunderbariger
    Meem

--- Finanzberg/resources/js/chart_script.js
+++ Finanzberg/resources/js/chart_script.js
@@ -1,43 +1,58 @@
 send("GET", "data/saldo", {}, function(response, status){
     const data = JSON.parse(response)
-    createChart(data.labels, data.data)
+    let dates = [];
+    data.labels.forEach((value) => { // Only one graph is needed for the labels
+        let date = new Date(value * 1000)
+        let name = date.toLocaleDateString() + " " + date.toLocaleTimeString()
+        dates.push(name)
+    })
+    createChart(data.dates, data.data)
 })
 
-const config = {
-    type: 'line',
-    data: data,
-    options: {
-      responsive: true,
-      plugins: {
-        title: {
-          display: true,
-          text: 'Chart.js Line Chart - Cubic interpolation mode'
-        },
-      },
-      interaction: {
-        intersect: false,
-      },
-      scales: {
-        x: {
-          display: true,
-          title: {
-            display: true
-          }
-        },
-        y: {
-          display: true,
-          title: {
-            display: true,
-            text: 'Value'
-          },
-          suggestedMin: -10,
-          suggestedMax: 200
-        }
-      }
-    },
-  };
+
 
 function createChart(labels, amounts) {
     const ctx = document.getElementById('myChart').getContext('2d');
-    new Chart(ctx,config);
+    const config = {
+        type: 'line',
+        data:{
+            labels: labels,
+            datasets: [
+                {
+                    label: "Saldo", 
+                    data: amounts
+                }
+            ]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: 'Chart.js Line Chart - Cubic interpolation mode'
+            },
+          },
+          interaction: {
+            intersect: false,
+          },
+          scales: {
+            x: {
+              display: true,
+              title: {
+                display: true
+              }
+            },
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Value'
+              },
+              suggestedMin: -10,
+              suggestedMax: 200
+            }
+          }
+        },
+      };
+    new Chart(ctx,config)
 }
